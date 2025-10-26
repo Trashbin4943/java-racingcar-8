25b0613652ddc7a5361e64b2dd5eb1b5df5e2f92
Trashbin4943 <anthonyhwan415@gmail.com>
2025-10-27 00:00:46 +0900

    FEAT: 시스템 출력 양식 형성
    OutputView: 명세서 4 (최종 우승자 정보 출력) 기반 시스템 출력 뷰 생성

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,36 @@
+package racingcar.view;
+import racingcar.domain.Car;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class OutputView {
+    private static final String POSITION_MARKER = "_";
+    private static final String WINNER_DELIMITER = ", ";
+
+    private static final String EXECUTION_RESULT_HEADER = "\n실행 결과";
+    private static final String CAR_STATUS_FORMAT = "%s : %s";
+    private static final String FINAL_WINNER_HEADER = "최종 우승자 : ";
+
+    public void printExecutionResultHeader() {
+        System.out.println(EXECUTION_RESULT_HEADER);
+    }
+
+    public void printRoundResult(List<Car> cars) {
+        for (Car car : cars) {
+            String positionString = generatePositionString(car.getPosition());
+            System.out.printf(CAR_STATUS_FORMAT + "%n", car.getName(), positionString);
+        }
+        System.out.println();
+    }
+
+    public void printFinalWinners(List<String> winners) {
+        String winnerNames = String.join(WINNER_DELIMITER, winners);
+
+        System.out.println(FINAL_WINNER_HEADER + winnerNames);
+    }
+
+    private String generatePositionString(int position) {
+        return POSITION_MARKER.repeat(position);
+    }
+}
